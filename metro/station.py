7f5dec658bbc5bfa21f3d93a040513370e6e6eaf
Alejandro Mallea <janoma@gmail.com>
2020-07-11 19:09:16 -0700

    Reformat of a couple of comments.

--- metro/station.py
+++ metro/station.py
@@ -20,14 +20,14 @@ class Station:
         self.color = colorFromStr(color)
 
     def __eq__(self, other):
-        """Why this is not the default behavior is beyond me,
-        but certainly something to investigate."""
+        # Why this is not the default behavior is beyond me,
+        # but certainly something to investigate (new to python classes).
         if not isinstance(other, Station):
             return NotImplemented
 
         return self.name == other.name and self.color == other.color
 
     def __hash__(self):
-        """Why this is not the default behavior is beyond me,
-        but certainly something to investigate."""
+        # Why this is not the default behavior is beyond me,
+        # but certainly something to investigate (new to python classes).
         return hash((self.name, self.color))
